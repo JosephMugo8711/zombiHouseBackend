e545955141528251551839d8bc2555a37a1154a1
MuthonduG <muthondugithinji@gmail.com>
2023-05-10 00:59:20 +0300

    Controllers

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,2 +1,55 @@
 class BuyersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :buyer_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+    rescue_from ActionController::UnpermittedParameters, with: :handle_errors
+
+    def index 
+        render json: Buyer.all, status: :ok
+    end
+
+    def show
+        buyer = find_buyer
+        render json: buyer, status: :ok
+    end
+
+    def create
+        buyer = Buyer.create!(buyer_params)
+        render json: buyer, status: :created
+    end
+
+    def update
+        buyer = find_buyer
+        buyer.update!(buyer_params)
+        render json: buyer, status: :created
+    end
+
+    def destroy
+        buyer = find_buyer
+        buyer.destroy
+        head: no_content
+    end
+
+    private
+
+    def find_buyer
+        Buyer.find(params[:id])
+    end 
+
+    def buyer_not_found
+        render json: { error: "buyer not found" }, status: :not_found
+    end
+
+    def buyer_params
+        params.require(:buyer).permit(:username, :fullname, :email, :contact, :avatar, :password)
+    end
+
+    def unprocessable_entity(invalid)
+        render json: { error: invalid.record.errors.fullmessages }, status: :unprocessable_entity
+    end
+
+    def handle_errors
+        render json: { "Unpermitted Parameters".params.to_unsafe_h.except(:controller, :action, :id, :username, :fullname, :email, :contact, :avatar, :password).keys}, status: :unprocessable_entity
+    end
+
 end
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,54 @@
 class PropertiesController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :property_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+    rescue_from ActionController::UnpermittedParameters, with: :handle_errors
+
+    def index 
+        render json: Property.all, status: :ok
+    end
+
+    def show
+        property = find_property
+        render json: property, status: :ok
+    end
+
+    def create
+        property = Property.create!(property_params)
+        render json: property, status: :created
+    end
+
+    def update
+        property = find_property
+        property.update!(property_params)
+        render json: property, status: :created
+    end
+
+    def destroy
+        property = find_property
+        property.destroy
+        head: no_content
+    end
+
+    private
+
+    def find_property
+        Property.find(params[:id])
+    end 
+
+    def property_not_found
+        render json: { error: "property not found" }, status: :not_found
+    end
+
+    def property_params
+        params.require(:property).permit(:username, :fullname, :email, :contact, :avatar, :password)
+    end
+
+    def unprocessable_entity(invalid)
+        render json: { error: invalid.record.errors.fullmessages }, status: :unprocessable_entity
+    end
+
+    def handle_errors
+        render json: { "Unpermitted Parameters".params.to_unsafe_h.except(:controller, :action, :id, :username, :fullname, :email, :contact, :avatar, :password).keys}, status: :unprocessable_entity
+    end
 end
